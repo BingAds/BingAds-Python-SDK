c3743ec036ab6cf8bbb0ffa3f3ff75a16c5aa6d3
Eric Urban <eur@microsoft.com>
2016-09-07 19:07:59 -0700

    draft of bulk extensions

--- examples/BingAdsPythonConsoleExamples/BingAdsPythonConsoleExamples/v10/AdExtensions.py
+++ examples/BingAdsPythonConsoleExamples/BingAdsPythonConsoleExamples/v10/AdExtensions.py
@@ -377,12 +377,13 @@ def output_ad_extension(extension):
 
 def output_schedule(schedule):
     if schedule is not None:
-        for day_time in schedule.DayTimeRanges['DayTime']:
-            output_status_message("Day: {0}".format(day_time.Day))
-            output_status_message("EndHour: {0}".format(day_time.EndHour))
-            output_status_message("EndMinute: {0}".format(day_time.EndMinute))
-            output_status_message("StartHour: {0}".format(day_time.StartHour))
-            output_status_message("StartMinute: {0}".format(day_time.StartMinute))
+        if schedule.DayTimeRanges is not None:
+            for day_time in schedule.DayTimeRanges['DayTime']:
+                output_status_message("Day: {0}".format(day_time.Day))
+                output_status_message("EndHour: {0}".format(day_time.EndHour))
+                output_status_message("EndMinute: {0}".format(day_time.EndMinute))
+                output_status_message("StartHour: {0}".format(day_time.StartHour))
+                output_status_message("StartMinute: {0}".format(day_time.StartMinute))
         if schedule.EndDate is not None:
             output_status_message(("EndDate: {0}/{1}/{2}".format( 
             schedule.EndDate.Month,
