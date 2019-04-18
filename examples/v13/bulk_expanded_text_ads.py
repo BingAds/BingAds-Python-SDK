f2c292a272b3f88b19a22b20032e22ea10f353b6
Eric Urban <eur@microsoft.com>
2019-04-18 06:44:12 -0700

    add v13 samples

---
+++ examples/v13/bulk_expanded_text_ads.py
@@ -0,0 +1,151 @@
+from auth_helper import *
+from bulk_service_manager_helper import *
+from output_helper import *
+
+# You must provide credentials in auth_helper.py.
+
+def main(authorization_data):
+
+    errors=[]
+
+    try:
+        # Add a search campaign.
+
+        upload_entities=[]
+                
+        bulk_campaign=BulkCampaign()
+        bulk_campaign.client_id='YourClientIdGoesHere'
+        campaign=set_elements_to_none(campaign_service.factory.create('Campaign'))
+        campaign.BudgetType='DailyBudgetStandard'
+        campaign.DailyBudget=50
+        languages=campaign_service.factory.create('ns3:ArrayOfstring')
+        languages.string.append('All')
+        campaign.Languages=languages
+        campaign.Name="Women's Shoes " + strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
+        campaign.TimeZone='PacificTimeUSCanadaTijuana'
+        campaign.Id=CAMPAIGN_ID_KEY
+        bulk_campaign.campaign=campaign       
+        upload_entities.append(bulk_campaign)
+
+        # Add an ad group within the campaign.
+        
+        bulk_ad_group=BulkAdGroup()
+        bulk_ad_group.campaign_id=CAMPAIGN_ID_KEY
+        ad_group=set_elements_to_none(campaign_service.factory.create('AdGroup'))
+        ad_group.Id=AD_GROUP_ID_KEY
+        ad_group.Name="Women's Red Shoe Sale"
+        end_date=campaign_service.factory.create('Date')
+        end_date.Day=31
+        end_date.Month=12
+        end_date.Year=strftime("%Y", gmtime())
+        ad_group.EndDate=end_date
+        cpc_bid=campaign_service.factory.create('Bid')
+        cpc_bid.Amount=0.09
+        ad_group.CpcBid=cpc_bid
+        bulk_ad_group.ad_group=ad_group
+        upload_entities.append(bulk_ad_group)
+
+        # Add keywords and ads within the ad group.
+        
+        bulk_keyword=BulkKeyword()
+        bulk_keyword.ad_group_id=AD_GROUP_ID_KEY
+        keyword=set_elements_to_none(campaign_service.factory.create('Keyword'))
+        keyword.Bid=set_elements_to_none(campaign_service.factory.create('Bid'))
+        keyword.Bid.Amount=0.47
+        keyword.Param2='10% Off'
+        keyword.MatchType='Broad'
+        keyword.Text='Brand-A Shoes'
+        bulk_keyword.keyword=keyword
+        upload_entities.append(bulk_keyword) 
+
+        bulk_expanded_text_ad=BulkExpandedTextAd()
+        bulk_expanded_text_ad.ad_group_id=AD_GROUP_ID_KEY
+        expanded_text_ad=set_elements_to_none(campaign_service.factory.create('ExpandedTextAd'))
+        expanded_text_ad.TitlePart1='Contoso'
+        expanded_text_ad.TitlePart2='Quick & Easy Setup'
+        expanded_text_ad.TitlePart3='Seemless Integration'
+        expanded_text_ad.Text='Find New Customers & Increase Sales!'
+        expanded_text_ad.TextPart2='Start Advertising on Contoso Today.'
+        expanded_text_ad.Path1='seattle'
+        expanded_text_ad.Path2='shoe sale'
+        expanded_text_ad.Type='ExpandedText'
+        final_urls=campaign_service.factory.create('ns3:ArrayOfstring')
+        final_urls.string.append('http://www.contoso.com/womenshoesale')
+        expanded_text_ad.FinalUrls=final_urls
+        bulk_expanded_text_ad.ad=expanded_text_ad
+        upload_entities.append(bulk_expanded_text_ad)
+                 
+        # Upload and write the output
+        
+        output_status_message("-----\nAdding campaign, ad group, keyword, and ad...")
+        download_entities=write_entities_and_upload_file(
+            bulk_service_manager=bulk_service_manager, 
+            upload_entities=upload_entities)
+
+        output_status_message("Upload results:")
+
+        campaign_results=[]
+
+        for entity in download_entities:
+            if isinstance(entity, BulkCampaign):
+                campaign_results.append(entity)
+                output_bulk_campaigns([entity])
+            if isinstance(entity, BulkAdGroup):
+                output_bulk_ad_groups([entity])
+            if isinstance(entity, BulkExpandedTextAd):
+                output_bulk_expanded_text_ads([entity])
+            if isinstance(entity, BulkKeyword):
+                output_bulk_keywords([entity])
+
+        # Delete the campaign and everything it contains e.g., ad groups and ads.
+                
+        upload_entities=[]
+
+        for campaign_result in campaign_results:
+            campaign_result.campaign.Status='Deleted'
+            upload_entities.append(campaign_result)
+            
+        output_status_message("-----\nDeleting the campaign and everything it contains e.g., ad groups and ads...")
+        download_entities=write_entities_and_upload_file(
+            bulk_service_manager=bulk_service_manager, 
+            upload_entities=upload_entities)
+
+        output_status_message("Upload results:")
+
+        for entity in download_entities:
+            if isinstance(entity, BulkCampaign):
+                output_bulk_campaigns([entity])
+
+    except WebFault as ex:
+        output_webfault_errors(ex)
+    except Exception as ex:
+        output_status_message(ex)
+
+# Main execution
+if __name__ == '__main__':
+
+    print("Loading the web service client proxies...")
+    
+    authorization_data=AuthorizationData(
+        account_id=None,
+        customer_id=None,
+        developer_token=DEVELOPER_TOKEN,
+        authentication=None,
+    )
+
+    bulk_service_manager=BulkServiceManager(
+        authorization_data=authorization_data, 
+        poll_interval_in_milliseconds=5000, 
+        environment=ENVIRONMENT,
+    )
+
+    campaign_service=ServiceClient(
+        service='CampaignManagementService', 
+        version=13,
+        authorization_data=authorization_data, 
+        environment=ENVIRONMENT,
+    )
+
+    authenticate(authorization_data)
+        
+    main(authorization_data)
---
+++ examples/v13/negative_keywords.py
@@ -0,0 +1,174 @@
+from auth_helper import *
+from output_helper import *
+
+# You must provide credentials in auth_helper.py.
+
+def main(authorization_data):
+
+    try:
+        # Add a campaign that will later be associated with negative keywords. 
+
+        campaigns=campaign_service.factory.create('ArrayOfCampaign')
+        campaign=set_elements_to_none(campaign_service.factory.create('Campaign'))
+        campaign.BudgetType='DailyBudgetStandard'
+        campaign.DailyBudget=50
+        languages=campaign_service.factory.create('ns3:ArrayOfstring')
+        languages.string.append('All')
+        campaign.Languages=languages
+        campaign.Name="Women's Shoes " + strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
+        campaign.TimeZone='PacificTimeUSCanadaTijuana'
+        campaigns.Campaign.append(campaign)
+
+        output_status_message("-----\nAddCampaigns:")
+        add_campaigns_response=campaign_service.AddCampaigns(
+            AccountId=authorization_data.account_id,
+            Campaigns=campaigns
+        )
+        campaign_ids={
+            'long': add_campaigns_response.CampaignIds['long'] if add_campaigns_response.CampaignIds['long'] else None
+        }
+        output_status_message("CampaignIds:")
+        output_array_of_long(campaign_ids)
+        output_status_message("PartialErrors:")
+        output_array_of_batcherror(add_campaigns_response.PartialErrors)
+
+        # You may choose to associate an exclusive set of negative keywords to an individual campaign 
+        # or ad group. An exclusive set of negative keywords cannot be shared with other campaigns 
+        # or ad groups. This example only associates negative keywords with a campaign.
+
+        entity_negative_keywords=campaign_service.factory.create('ArrayOfEntityNegativeKeyword')
+        entity_negative_keyword=set_elements_to_none(campaign_service.factory.create('EntityNegativeKeyword'))
+        entity_negative_keyword.EntityId=campaign_ids['long'][0]
+        entity_negative_keyword.EntityType="Campaign"
+        negative_keywords=campaign_service.factory.create('ArrayOfNegativeKeyword')
+        auto_exact=campaign_service.factory.create('NegativeKeyword')
+        auto_exact.MatchType='Exact'
+        auto_exact.Text="auto"
+        negative_keywords.NegativeKeyword.append(auto_exact)
+        auto_phrase=campaign_service.factory.create('NegativeKeyword')
+        auto_phrase.MatchType='Phrase'
+        auto_phrase.Text="auto"
+        negative_keywords.NegativeKeyword.append(auto_phrase)
+        entity_negative_keyword.NegativeKeywords=negative_keywords
+        entity_negative_keywords.EntityNegativeKeyword.append(entity_negative_keyword)
+
+        output_status_message("-----\nAddNegativeKeywordsToEntities:")
+        add_negative_keywords_to_entities_response=campaign_service.AddNegativeKeywordsToEntities(
+            EntityNegativeKeywords=entity_negative_keywords
+        )
+        output_status_message("Added an exclusive set of negative keywords to the Campaign.")
+        output_status_message("NegativeKeywordIds:")
+        output_array_of_idcollection(add_negative_keywords_to_entities_response.NegativeKeywordIds)
+        output_status_message("NestedPartialErrors:")
+        output_array_of_batcherrorcollection(add_negative_keywords_to_entities_response.NestedPartialErrors)
+        
+        # If you attempt to delete a negative keyword without an identifier the operation will return
+        # partial errors corresponding to the index of the negative keyword that was not deleted. 
+        
+        output_status_message("-----\nDeleteNegativeKeywordsFromEntities:")
+        nested_partial_errors=campaign_service.DeleteNegativeKeywordsFromEntities(
+            EntityNegativeKeywords=entity_negative_keywords
+        )
+        output_status_message("Attempt to DeleteNegativeKeywordsFromEntities without NegativeKeyword identifier partially fails by design.")
+        output_array_of_batcherrorcollection(nested_partial_errors)
+
+        # Negative keywords can also be added and deleted from a shared negative keyword list. 
+        # The negative keyword list can be shared or associated with multiple campaigns.
+        # NegativeKeywordList inherits from SharedList which inherits from SharedEntity.
+
+        negative_keyword_list=set_elements_to_none(campaign_service.factory.create('NegativeKeywordList'))
+        negative_keyword_list.Name="My Negative Keyword List " + strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
+        negative_keyword_list.Type="NegativeKeywordList"
+        
+        negative_keywords=campaign_service.factory.create('ArrayOfSharedListItem')
+        car_exact=set_elements_to_none(campaign_service.factory.create('NegativeKeyword'))
+        car_exact.Text="car"
+        car_exact.Type="NegativeKeyword"
+        car_exact.MatchType='Exact'
+        negative_keywords.SharedListItem.append(car_exact)
+        car_phrase=set_elements_to_none(campaign_service.factory.create('NegativeKeyword'))
+        car_phrase.Text="car"
+        car_phrase.Type="NegativeKeyword"
+        car_phrase.MatchType='Phrase'
+        negative_keywords.SharedListItem.append(car_phrase)
+
+        # You can create a new list for negative keywords with or without negative keywords.
+
+        output_status_message("-----\nAddSharedEntity:")
+        add_shared_entity_response=campaign_service.AddSharedEntity(
+            SharedEntity=negative_keyword_list, 
+            ListItems=negative_keywords
+        )
+        shared_entity_id=add_shared_entity_response.SharedEntityId
+        list_item_ids=add_shared_entity_response.ListItemIds
+        output_status_message("NegativeKeywordList added to account library and assigned identifer {0}".format(shared_entity_id))
+        
+        # Negative keywords were added to the negative keyword list above. You can associate the 
+        # shared list of negative keywords with a campaign with or without negative keywords. 
+        # Shared negative keyword lists cannot be associated with an ad group. An ad group can only 
+        # be assigned an exclusive set of negative keywords. 
+
+        shared_entity_associations=campaign_service.factory.create('ArrayOfSharedEntityAssociation')
+        shared_entity_association=set_elements_to_none(campaign_service.factory.create('SharedEntityAssociation'))
+        shared_entity_association.EntityId=campaign_ids['long'][0]
+        shared_entity_association.EntityType="Campaign"
+        shared_entity_association.SharedEntityId=shared_entity_id
+        shared_entity_association.SharedEntityType="NegativeKeywordList" 
+        shared_entity_associations.SharedEntityAssociation.append(shared_entity_association)
+        
+        output_status_message("-----\nSetSharedEntityAssociations:")
+        partial_errors=campaign_service.SetSharedEntityAssociations(
+            Associations=shared_entity_associations)
+        output_status_message(
+            "Associated CampaignId {0} with Negative Keyword List Id {1}.".format(campaign_ids['long'][0], shared_entity_id)
+        )
+
+        # Delete the campaign and everything it contains e.g., ad groups and ads.
+
+        output_status_message("-----\nDeleteCampaigns:")
+        campaign_service.DeleteCampaigns(
+            AccountId=authorization_data.account_id,
+            CampaignIds=campaign_ids
+        )
+        output_status_message("Deleted Campaign Id {0}".format(campaign_ids['long'][0]))
+
+        # DeleteCampaigns does not delete the negative keyword list from the account's library. 
+        #/ Call the DeleteSharedEntities operation to delete the negative keyword list.
+
+        negative_keyword_lists=campaign_service.factory.create('ArrayOfSharedEntity')
+        negative_keyword_list=campaign_service.factory.create('NegativeKeywordList')
+        negative_keyword_list.Id=shared_entity_id
+        negative_keyword_lists.SharedEntity.append(negative_keyword_list)
+
+        output_status_message("-----\nDeleteSharedEntities:")
+        partial_errors=campaign_service.DeleteSharedEntities(
+            SharedEntities=negative_keyword_lists)
+        output_status_message("Deleted Negative Keyword List Id {0}".format(shared_entity_id))
+
+    except WebFault as ex:
+        output_webfault_errors(ex)
+    except Exception as ex:
+        output_status_message(ex)
+
+# Main execution
+if __name__ == '__main__':
+
+    print("Loading the web service client proxies...")
+    
+    authorization_data=AuthorizationData(
+        account_id=None,
+        customer_id=None,
+        developer_token=DEVELOPER_TOKEN,
+        authentication=None,
+    )
+
+    campaign_service=ServiceClient(
+        service='CampaignManagementService', 
+        version=13,
+        authorization_data=authorization_data, 
+        environment=ENVIRONMENT,
+    )
+        
+    authenticate(authorization_data)
+        
+    main(authorization_data)
