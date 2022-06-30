ab509f9bba26c2ebe8aa2d15aed9654f42ea84f7
Qingjun Tian <qitia@microsoft.com>
2022-06-30 14:32:25 +0800

    update doc to v13.0.14

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,12 +1,9 @@
 var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '13.0.13',
+    URL_ROOT: '',
+    VERSION: '13.0.14',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
-    BUILDER: 'html',
     FILE_SUFFIX: '.html',
-    LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false
+    SOURCELINK_SUFFIX: '.txt'
 };
