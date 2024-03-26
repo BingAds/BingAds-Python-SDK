74840af0dc1cef4df1bc87fe4c382a49198db094
Xinyu Wen <xinyuwen@microsoft.com>
2024-03-26 11:00:22 +0800

    update doc version to 13.0.19

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '13.0.15',
+    VERSION: '13.0.19',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
